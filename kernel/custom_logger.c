8a6c6fc5d8b1235df6383ff0c1fa9d88cb57abac
fatemeh <fatemehabdolhoseini257@gmail.com>
2025-04-17 20:12:29 +0330

    task in working...
     تغیرات لازم در فایل های زیر انجام شد
    syscall.h
    
    custom_logger.c
    
    custom_logger.h
    
    main.c

---
+++ kernel/custom_logger.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "logger.h"
+
+void log_message(log_level_t level, const char *message) {
+    switch (level) {
+        case LOG_INFO:
+            printf("[INFO] %s\n", message);
+            break;
+        case LOG_WARNING:
+            printf("\033[33m[WARNING] %s\033[0m\n", message); // رنگ زرد
+            break;
+        case LOG_ERROR:
+            printf("\033[31m[ERROR] %s\033[0m\n", message); // رنگ قرمز
+            break;
+    }
+}
